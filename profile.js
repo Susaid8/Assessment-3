4e78f6e26df9c979e4a8092c7f3f497b8037922c
Susaid8 <97367629+Susaid8@users.noreply.github.com>
2022-04-19 16:02:47 -0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,24 +1,9 @@
-const thisButton = document.querySelectorAll('#buttons')
+const thisButton = document.getElementsByClassName('this-button')
 
 for(let i = 0;i<thisButton.length;i++){
     thisButton[i].addEventListener('click', (e)=> {
         alert(`My favorite ${e.target.id} is ${e.target.value}`)
+       
     })
 }
 
-let buttons = document.querySelector('thisbutton')
-buttons = [...buttons.children]
-console.log(buttons)
-
-buttons.forEach(e => e.addEventListener('click', () => {
-    switch(e.id) {
-        case 'color':
-            alert('Black')
-            break;
-        case 'place':
-            alert('Bali')
-            break;
-        default:
-            alert('Praying')
-    }
-}))
